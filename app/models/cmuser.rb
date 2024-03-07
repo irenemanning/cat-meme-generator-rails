acb5f66e8ff3e665bf19f634dc33ba225d4911d5
Irene <irenemanningm@gmail.com>
2024-03-06 18:03:45 -0600

    Generate cmuser table and cmuser controller.

---
+++ app/models/cmuser.rb
@@ -0,0 +1,2 @@
+class Cmuser < ApplicationRecord
+end
---
+++ db/migrate/20240307000209_create_cmusers.rb
@@ -0,0 +1,10 @@
+class CreateCmusers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cmusers do |t|
+      t.string :username
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
