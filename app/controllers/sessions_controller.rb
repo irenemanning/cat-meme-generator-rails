5c63cf54da9d26cc3b4902f598e4b54be251ec25
Irene <irenemanningm@gmail.com>
2024-03-06 21:57:11 -0600

    Create Login and Logout function in session controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+    skip_before_action :authorize, only: :create
+  
+    def create
+      user = Cmuser.find_by(username: params[:username])
+      if user&.authenticate(params[:password])
+        session[:cmuser_id] = user.id
+        render json: user, status: :created
+      else
+        render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+      end
+    end
+  
+    def destroy
+      session.delete :cmuser_id
+      render json: { errors: ["Please Login Again"] }, status: :no_content
+    end
+
 end
