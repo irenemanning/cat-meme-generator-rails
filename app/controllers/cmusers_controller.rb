93d9afd13f2d287ed7903d561bfe6a3d6496c3de
Irene <irenemanningm@gmail.com>
2024-03-06 22:29:49 -0600

    Create a user sign up route and create method.

--- app/controllers/cmusers_controller.rb
+++ app/controllers/cmusers_controller.rb
@@ -1,2 +1,24 @@
 class CmusersController < ApplicationController
+    #  /me
+    def show
+        render json: @current_user
+    end
+
+    # /signup
+    def create
+        user = Cmuser.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:wuser).permit(:username, :password, :password_confirmation, :profile_image)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :meme_likes
   resources :memes
+  
+  get "/me", to: "cmusers#show"
+  post "/signup", to: "cmusers#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
 end
