20e83c99a5e1185c007f2f271ea9e98b00b1b6a8
Irene <irenemanningm@gmail.com>
2024-03-06 18:33:57 -0600

    Add model associations to cmuser, meme_like, and meme tables.

--- app/models/cmuser.rb
+++ app/models/cmuser.rb
@@ -1,3 +1,8 @@
 class Cmuser < ApplicationRecord
+    has_one_attached :profile_image
+    has_many :memes
+    has_many :likes
+
     has_secure_password
+    
 end
--- app/models/meme_like.rb
+++ app/models/meme_like.rb
@@ -1,2 +1,4 @@
 class MemeLike < ApplicationRecord
+    belongs_to :cmuser
+    belongs_to :meme
 end
