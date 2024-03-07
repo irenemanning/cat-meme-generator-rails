3c813d5a049850bc6b40bd302053015638a5a0b8
Irene <irenemanningm@gmail.com>
2024-03-06 19:26:06 -0600

    Configure middleware.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :meme_likes
   resources :memes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
