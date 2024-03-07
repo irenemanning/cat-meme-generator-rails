d8975f1e02697a19810fa275d9e77f3ffceb4fce
Irene <irenemanningm@gmail.com>
2024-03-06 18:00:51 -0600

    Rollback user table migration and controller generation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_06_235131) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_234138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,13 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_06_235131) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
