caa0f43c8463371138975662ce46b27b5ffa52fc
Irene <irenemanningm@gmail.com>
2024-05-17 00:46:20 -0500

    Add routing to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,26 @@ import '../App.css';
 import Navbar from './NavBar';
 import Home from './pages/Home'
 function App() {
+  // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+  const isAuthenticated = true
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
+          <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <Signup />} />
+          
+          {isAuthenticated ? (
+            <>
+              <Route path="/memes" element={} />
+              <Route path="/+meme" element={} />
+              <Route path="/profile" element={} />
+              {/* <Route path="/profile/settings" element={} /> */}
+            </>
+          ) : (
+            <Route path="/" element={<Home />} />
+          )}
         </Routes>
       </Router>
     </div>
