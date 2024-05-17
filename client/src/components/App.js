96ebff235c946b2f61083a2fbb88b774a1bed038
Irene <irenemanningm@gmail.com>
2024-05-17 00:54:50 -0500

    Create React functional components for  Memes, Profile, Create Meme, Login, and Signup pages.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,12 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import '../App.css';
-import Navbar from './NavBar';
+import '../App.css'
+import Navbar from './NavBar'
 import Home from './pages/Home'
+import Memes from './pages/Memes'
+import CreateMeme from './pages/CreateMeme'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 function App() {
   // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
   const isAuthenticated = true
@@ -11,13 +16,14 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
-          <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <Signup />} />
+          <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <SignUp />} />
+          <Route path="/" element={<Home />} />
           
           {isAuthenticated ? (
             <>
-              <Route path="/memes" element={} />
-              <Route path="/+meme" element={} />
-              <Route path="/profile" element={} />
+              <Route path="/memes" element={<Memes />} />
+              <Route path="/+meme" element={<CreateMeme />} />
+              <Route path="/profile" element={<Profile />} />
               {/* <Route path="/profile/settings" element={} /> */}
             </>
           ) : (
@@ -26,7 +32,7 @@ function App() {
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
